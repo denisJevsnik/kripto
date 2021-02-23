f386def92f7a112dc935d2e35ac9b48c159a5126
Denis Jevsnik <denis.jevsnik@gmail.com>
2021-02-23 17:29:20 +0100

    File Upload

--- cryptocurrencies_add.php
+++ cryptocurrencies_add.php
@@ -15,10 +15,10 @@
             <div class="row">
                 <div class="col-lg-8 mx-auto">
                     <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
-                    <form action="cryptocurrency_insert.php" method="post">
+                    <form action="cryptocurrency_insert.php" method="post" enctype="multipart/form-data">
 
                         <div class="control-group">
-                            <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <div class="form-group floating-label-form-group   mb-0 pb-2">
                                 <label>Ime</label>
                                 <input class="form-control" type="text" name="title" placeholder="Vnasite ime valute" required="required"/> <br/>
                             </div>
--- cryptocurrency_insert.php
+++ cryptocurrency_insert.php
@@ -7,31 +7,63 @@
        $title = $_POST['title'];
        $description = $_POST['description'];
        $current_price = floatval($_POST['current_price']);  // ce je float 0,2323(txt) -> ok; ce je false("shdlas") vrne 0
-       $logo = 'logotip';
+  
+        $target_dir = "uploads/";
+
+        $random = date('YmdHisu'); // 2021023170122
+
+        // logo->kazalec na datoteko, name-> ime datoteke
+        $target_file = $target_dir . $random . basename($_FILES["logo"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
    
+        // preveria ali ima datoteka dejansko velikost
+        $check = getimagesize($_FILES["logo"]["tmp_name"]);
+        if($check !== false) {
+          $uploadOk = 1;
+        } else {
+          $uploadOk = 0;
+        }
+
+        // Check file size max 5MB
+        if ($_FILES["logo"]["size"] > 5000000) {
+        $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+        $uploadOk = 0;
+        }
+
         // ki so Notnull
-       if (!empty($title) && !empty($logo))
+       if (!empty($title) && ($uploadOk == 1))
        {
+
+            if (move_uploaded_file($_FILES["logo"]["tmp_name"], $target_file)) {
+                // zapise se vse v bazo
+                //Definiramo poizvedbo
+                $query = "INSERT INTO cryptocurrencies(title, description, current_price, logo) VALUES(?, ?, ?, ?)";
    
-           //Definiramo poizvedbo
-           $query = "INSERT INTO cryptocurrencies(title, description, current_price, logo) VALUES(?, ?, ?, ?)";
+                //Izvrsitvenemu stavku dolocimo da na DB pripravi poizvedbo
+                $stmt = $pdo->prepare($query);
+                //In jo izvrsi z vrednostmi prejetih z metodo _POST
+                $stmt->execute([$title,$description,$current_price,$target_file]);
    
-           //Izvrsitvenemu stavku dolocimo da na DB pripravi poizvedbo
-           $stmt = $pdo->prepare($query);
-           //In jo izvrsi z vrednostmi prejetih z metodo _POST
-           $stmt->execute([$title,$description,$current_price,$logo]);
-   
-           //Po registraciji nadalnjujemo na strani 
-           header("Location: cryptocurrencies.php");
-           die();
+                 //Po registraciji nadalnjujemo na strani 
+                 header("Location: cryptocurrencies.php");
+                 die();
+
+             } else {
+                //Ce pravila vnosa niso izponjena po response ostanemo na strani register.php
+                 header("Location: cryptocurrencies_add.php");
+                die();
+             }
    
        } else {
            //Ce pravila vnosa niso izponjena po response ostanemo na strani register.php
            header("Location: cryptocurrencies_add.php");
            die();
        }
- 
-
-
 
 ?>
